123c93b7126099ce05bf8966bc783b83066aff87
daltonlin <>
2020-04-26 23:46:14 -0700

    user log in/out and register

--- final/final/settings.py
+++ final/final/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calculator',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = "calculator.User"
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
